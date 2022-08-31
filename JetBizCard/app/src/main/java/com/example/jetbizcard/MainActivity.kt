cc01ed6524857ac0c2f3688d68b370c2a043c4a2
Rafal Mi <rafal.m4@gmail.com>
2022-08-31 23:14:55 +0200

    14. Creating a Card ...

--- JetBizCard/app/src/main/java/com/example/jetbizcard/MainActivity.kt
+++ JetBizCard/app/src/main/java/com/example/jetbizcard/MainActivity.kt
@@ -3,15 +3,18 @@ package com.example.jetbizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetbizcard.ui.theme.JetBizCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,7 +38,15 @@ class MainActivity : ComponentActivity() {
 fun CreateBizCard() {
     Surface(modifier = Modifier.fillMaxWidth()
         .fillMaxHeight()) {
+        Card(modifier = Modifier
+//            .width(200.dp)
+//            .height(390.dp)
+            .padding(12.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+            elevation = 4.dp
+        ) {
 
+        }
     }
 }
 
