f9f8de0ab9843daf1512455c7f86a1bf1580e1a7
Rafal Mi <rafal.m4@gmail.com>
2022-09-07 00:25:16 +0200

    15. Adding the Profile Image

--- JetBizCard/app/src/main/java/com/example/jetbizcard/MainActivity.kt
+++ JetBizCard/app/src/main/java/com/example/jetbizcard/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.jetbizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -13,6 +16,8 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetbizcard.ui.theme.JetBizCardTheme
@@ -36,7 +41,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard() {
-    Surface(modifier = Modifier.fillMaxWidth()
+    Surface(modifier = Modifier
+        .fillMaxWidth()
         .fillMaxHeight()) {
         Card(modifier = Modifier
 //            .width(200.dp)
@@ -45,7 +51,26 @@ fun CreateBizCard() {
             shape = RoundedCornerShape(corner = CornerSize(15.dp)),
             elevation = 4.dp
         ) {
+            Surface(
+                modifier = Modifier
+                    .size(150.dp)
+                    .padding(5.dp),
+                shape = CircleShape,
+                border = BorderStroke(
+                    width = 0.5.dp,
+                    color = Color.LightGray
+                ),
+                elevation = 4.dp,
+                color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)
+            ) {
+               Image(
+                   painter = painterResource(id = R.drawable.profile_image),
+                   contentDescription = "profile image",
+                   modifier = Modifier.size(135.dp),
+                   contentScale = ContentScale.Crop
 
+               )
+            }
         }
     }
 }
