7b4c9138833d4818505ee9e2119d2dd87ddd8f05
Rafal Mi <rafal.m4@gmail.com>
2022-09-24 21:58:28 +0200

    Upgraded Coil, Kotlin, Compose compiler, but API too different

--- MovieApp/app/src/main/java/com/example/movieapp/widgets/widgets.kt
+++ MovieApp/app/src/main/java/com/example/movieapp/widgets/widgets.kt
@@ -1,24 +1,28 @@
 package com.example.movieapp.widgets
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
 import com.example.movieapp.model.Movie
+import com.example.movieapp.model.getMovies
 
+@Preview
 @Composable
-fun MovieRow(movie: Movie, onItemClick: (String) -> Unit = {}) {
+fun MovieRow(movie: Movie = getMovies()[0],
+             onItemClick: (String) -> Unit = {}) {
     Card(modifier = Modifier
         .padding(4.dp)
         .fillMaxWidth()
@@ -36,11 +40,24 @@ fun MovieRow(movie: Movie, onItemClick: (String) -> Unit = {}) {
                 .size(100.dp),
                 shape = RectangleShape,
                 elevation = 4.dp) {
-                Icon(imageVector = Icons.Default.AccountBox,
-                    contentDescription = "Movie Image")
+//                Image(painter = rememberAsyncImagePainter(model = movie.images[0],
+//                    builder),
+//                    contentDescription = "Movie Poster")
+
+                Image(painter = reme)
+//                Icon(imageVector = Icons.Default.AccountBox,
+//                    contentDescription = "Movie Image")
 
             }
-            Text(text = movie.title)
+            Column() {
+                Text(text = movie.title,
+                    style = MaterialTheme.typography.h6)
+                Text(text = "Director: ${movie.director}",
+                    style = MaterialTheme.typography.caption)
+                Text(text = "Released: ${movie.year}",
+                    style = MaterialTheme.typography.caption)
+            }
+
 
         }
     }
