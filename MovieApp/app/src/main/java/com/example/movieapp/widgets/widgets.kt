76cf9cfc0487d144805280457ea01d34b3626cab
Rafal Mi <rafal.m4@gmail.com>
2022-09-25 15:54:16 +0200

    115. Expand Capability

--- MovieApp/app/src/main/java/com/example/movieapp/widgets/widgets.kt
+++ MovieApp/app/src/main/java/com/example/movieapp/widgets/widgets.kt
@@ -1,5 +1,6 @@
 package com.example.movieapp.widgets
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -8,12 +9,20 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.rememberImagePainter
 import coil.transform.CircleCropTransformation
 import com.example.movieapp.model.Movie
@@ -23,10 +32,13 @@ import com.example.movieapp.model.getMovies
 @Composable
 fun MovieRow(movie: Movie = getMovies()[0],
              onItemClick: (String) -> Unit = {}) {
+    var expanded by remember {
+        mutableStateOf(false)
+    }
     Card(modifier = Modifier
         .padding(4.dp)
         .fillMaxWidth()
-        .height(130.dp)
+        // .height(130.dp)
         .clickable {
             onItemClick(movie.id)
         },
@@ -58,8 +70,37 @@ fun MovieRow(movie: Movie = getMovies()[0],
                     style = MaterialTheme.typography.caption)
                 Text(text = "Released: ${movie.year}",
                     style = MaterialTheme.typography.caption)
-            }
 
+                AnimatedVisibility(visible = expanded) {
+                    Column() {
+                        Text(
+                            buildAnnotatedString {
+                                withStyle(style = SpanStyle(color = Color.DarkGray,
+                                    fontSize = 13.sp)) {
+                                    append("Plot: ")
+                                }
+                                withStyle(style = SpanStyle(color = Color.DarkGray,
+                                    fontSize = 13.sp,
+                                    fontWeight = FontWeight.Light
+                                )) {
+                                    append(movie.plot)
+                                }
+
+                            }
+                        )
+                    }
+                }
+
+                Icon(imageVector = if(expanded) Icons.Filled.KeyboardArrowUp else
+                    Icons.Filled.KeyboardArrowDown,
+                    contentDescription = "Down Arrow",
+                    modifier = Modifier
+                        .size(25.dp)
+                        .clickable {
+                            expanded = !expanded
+                        },
+                    tint = Color.DarkGray)
+            }
         }
     }
 }
