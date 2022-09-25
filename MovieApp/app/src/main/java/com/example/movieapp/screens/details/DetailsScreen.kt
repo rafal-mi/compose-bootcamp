37b7c709e618267d78abca7bdbaf32476d9de003
Rafal Mi <rafal.m4@gmail.com>
2022-09-25 17:25:48 +0200

    117. Finishing up the Details Screen

--- MovieApp/app/src/main/java/com/example/movieapp/screens/details/DetailsScreen.kt
+++ MovieApp/app/src/main/java/com/example/movieapp/screens/details/DetailsScreen.kt
@@ -1,7 +1,11 @@
 package com.example.movieapp.screens.details
 
+import android.widget.Space
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -9,14 +13,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.semantics.Role.Companion.Button
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
+import com.example.movieapp.model.Movie
+import com.example.movieapp.model.getMovies
+import com.example.movieapp.widgets.MovieRow
 
 @Composable
 fun DetailsScreen(navController: NavController,
-                  movieData: String?) {
-
+                  movieId: String?) {
+    val newMovieList = getMovies().filter { movie ->
+        movie.id == movieId
+    }
     Scaffold(topBar = {
         TopAppBar(backgroundColor = Color.LightGray,
             elevation = 5.dp
@@ -38,11 +48,36 @@ fun DetailsScreen(navController: NavController,
             .fillMaxHeight()
             .fillMaxWidth()) {
 
-            Column(horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center) {
+            HorizontalScrollableImageView(newMovieList)
+        }
+    }
+}
 
-                Text(text = movieData.toString(),
-                    style = MaterialTheme.typography.h5)
+@Composable
+private fun HorizontalScrollableImageView(newMovieList: List<Movie>) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+
+
+        MovieRow(movie = newMovieList.first())
+        Spacer(modifier = Modifier.height(8.dp))
+        Divider()
+        Text(text = "Movie Imagers")
+        LazyRow() {
+            items(newMovieList[0].images) { image ->
+                Card(
+                    modifier = Modifier
+                        .padding(12.dp)
+                        .size(240.dp),
+                    elevation = 5.dp
+                ) {
+                    Image(
+                        painter = rememberImagePainter(data = image),
+                        contentDescription = "Movie Poster"
+                    )
+                }
             }
         }
     }
