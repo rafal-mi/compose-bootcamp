d28c1d93cd88d6f798cb3d728c75328acae02daf
Rafal Mi <rafal.m4@gmail.com>
2022-09-26 20:25:14 +0200

    126. ViewModel

--- NoteApp/app/src/main/java/com/example/noteapp/MainActivity.kt
+++ NoteApp/app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.noteapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -13,9 +14,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.noteapp.data.NotesDataSource
 import com.example.noteapp.model.Note
 import com.example.noteapp.screen.NoteScreen
+import com.example.noteapp.screen.NoteViewModel
 import com.example.noteapp.ui.theme.NoteAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,14 +31,8 @@ class MainActivity : ComponentActivity() {
                     // modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    val notes = remember {
-                        mutableStateListOf<Note>()
-                    }
-                    NoteScreen(notes = notes, onAddNote = {
-                        notes.add(it)
-                    }, onRemoveNote = {
-                        notes.remove(it)
-                    })
+                    val noteViewModel: NoteViewModel by viewModels()
+                    NotesApp(noteViewModel)
                 }
             }
         }
@@ -44,6 +41,13 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
+}
+
+@Composable
+fun NotesApp(noteViewModel: NoteViewModel = viewModel()) {
+    val notesList = noteViewModel.getAllNotes()
+    NoteScreen(notes = notesList, onAddNote = { noteViewModel.addNote(it) },
+        onRemoveNote = { noteViewModel.removeNote(it) })
 }
 
 @Preview(showBackground = true)
