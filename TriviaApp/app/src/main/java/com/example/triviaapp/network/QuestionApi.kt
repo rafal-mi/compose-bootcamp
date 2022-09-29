d952736903cfb368c2309c3dbfb376d4077a800d
Rafal Mi <rafal.m4@gmail.com>
2022-09-29 14:53:39 +0200

    151.Wrapper Class

---
+++ TriviaApp/app/src/main/java/com/example/triviaapp/network/QuestionApi.kt
@@ -0,0 +1,9 @@
+package com.example.triviaapp.network
+
+import com.example.triviaapp.model.Question
+import retrofit2.http.GET
+
+interface QuestionApi {
+    @GET("world.json")
+    suspend fun getAllQuestions(): Question
+}
