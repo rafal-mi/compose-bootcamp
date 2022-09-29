f408bae18b80f22a2eff3a739e1604f08f993bfe
Rafal Mi <rafal.m4@gmail.com>
2022-09-29 20:34:16 +0200

    154. Testing VieModel and Logging

--- TriviaApp/app/src/main/java/com/example/triviaapp/MainActivity.kt
+++ TriviaApp/app/src/main/java/com/example/triviaapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.triviaapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,8 +11,13 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.triviaapp.TriviaApplication.Companion.TAG
+import com.example.triviaapp.screens.QuestionsViewModel
 import com.example.triviaapp.ui.theme.TriviaAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,10 +25,10 @@ class MainActivity : ComponentActivity() {
             TriviaAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    // modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    TriviaHome()
                 }
             }
         }
@@ -30,14 +36,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TriviaHome(viewModel: QuestionsViewModel = hiltViewModel()) {
+    Questions(viewModel)
+
+}
+
+@Composable
+fun Questions(viewModel: QuestionsViewModel) {
+    val questions = viewModel.data.value.data?.toMutableList()
+    if(viewModel.data.value.loading == true) {
+        Log.d(TAG, "Questions: ...Loading...")
+    } else {
+        questions?.forEach { questionItem ->
+            Log.d(TAG, "Question: ${questionItem.question}")
+        }
+    }
+
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TriviaAppTheme {
-        Greeting("Android")
     }
 }
