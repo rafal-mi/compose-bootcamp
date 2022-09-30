7265a74e5df9af32c8d180c784b5859967c34941
Rafal Mi <rafal.m4@gmail.com>
2022-09-30 21:02:51 +0200

    162. Score Meter

--- TriviaApp/app/src/main/java/com/example/triviaapp/component/Questions.kt
+++ TriviaApp/app/src/main/java/com/example/triviaapp/component/Questions.kt
@@ -7,10 +7,8 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material.ButtonDefaults.buttonColors
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -22,12 +20,14 @@ import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextIndent
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.triviaapp.TriviaApplication
+import com.example.triviaapp.TriviaApplication.Companion.TAG
 import com.example.triviaapp.model.QuestionItem
 import com.example.triviaapp.screens.QuestionsViewModel
 import com.example.triviaapp.util.AppColors
@@ -91,9 +91,13 @@ fun QuestionDisplay(
             .fillMaxHeight(),
         color = AppColors.mDarkPurple
     ) {
-        Column(modifier = Modifier.padding(12.dp),
+        Column(
+            modifier = Modifier.padding(12.dp),
             verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.Start) {
+            horizontalAlignment = Alignment.Start
+        ) {
+
+            if(questionIndex.value >= 3) ShowProgress(score = questionIndex.value)
             QuestionTracker(counter = questionIndex.value)
             DrawDottedLine(pathEffect = pathEffect)
 
@@ -198,6 +202,69 @@ fun DrawDottedLine(pathEffect: PathEffect) {
             start = Offset(x = 0f, y = 0f),
             end = Offset(x = size.width, y = 0f),
             pathEffect = pathEffect)
+    }
+}
+
+@Preview
+@Composable
+fun ShowProgress(score: Int = 12) {
+    val gradient = Brush.linearGradient(
+        listOf(
+            Color(0xFFF95075), Color(0xFFBE6Be5)
+        )
+    )
+    val progressFactor by remember(score) {
+        mutableStateOf(score*0.005f)
+    }
+    Row(
+        modifier = Modifier
+            .padding(3.dp)
+            .fillMaxWidth()
+            .height(45.dp)
+            .border(
+                width = 4.dp,
+                brush = Brush.linearGradient(
+                    colors = listOf(AppColors.mLightPurple, AppColors.mLightPurple)
+                ),
+                shape = RoundedCornerShape(34.dp)
+            )
+            .clip(
+                RoundedCornerShape(
+                    topStartPercent = 50,
+                    topEndPercent = 50,
+                    bottomStartPercent = 50,
+                    bottomEndPercent = 50
+                )
+            )
+            .background(Color.Transparent),
+        verticalAlignment = Alignment.CenterVertically
+
+    ) {
+        Log.d(TAG, "Progress factor is $progressFactor")
+        Button(
+            contentPadding = PaddingValues(1.dp),
+            onClick = {},
+            modifier = Modifier
+                .fillMaxWidth(progressFactor)
+                .background(brush = gradient),
+            enabled = false,
+            elevation = null,
+            colors = buttonColors(
+                backgroundColor = Color.Transparent,
+                disabledBackgroundColor = Color.Transparent
+            )
+        ) {
+            Text(
+                text = (score * 10).toString(),
+                modifier = Modifier
+                    .clip(shape = RoundedCornerShape(23.dp))
+                    .fillMaxHeight(0.87f)
+                    .fillMaxWidth()
+                    .padding(6.dp),
+                color = AppColors.mOffWhite,
+                textAlign = TextAlign.Center
+            )
+        }
     }
 }
 
